93ebc98f1fd2227c98a61fbdb9b89b81f5b5618b
blueskymana <1024791744@qq.com>
2017-07-04 18:57:53 +0800

    init resp.

---
+++ q_rsqrt.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+float Q_rsqrt(float number) {
+    long i;
+    float x2, y;
+    float threehalfs = 1.5f;
+    
+    x2 = number * 0.5f;
+    y = number;
+    i = *(long*)&y;
+    i = 0x5f3759df - (i >> 1);
+    y = *(float*)&i;
+    y = y*(threehalfs - (x2*y*y));
+    return y;
+}
+
+int main() {
+    printf("%f\n", Q_rsqrt(2.0f));
+    return 0;
+}
